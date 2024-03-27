e14a6a7a1a12c60ff5ae35c9c5474a96a0755a7f
Carlos J Guzman <152306129+cjavier7@users.noreply.github.com>
2024-03-27 15:44:21 +0000

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import injectContext from './store/AppContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import DetailedPeople from './pages/DetailedPeople';
+import DetailedPlanets from './pages/DetailedPlanets';
+import DetailedVehicles from './pages/DetailedVehicles';
+import { Context } from './store/AppContext';
+
 
 const App = () => {
+    
+    const { store, actions } = useContext(Context)
+
     return (
-        <h1>REACT APP</h1>
+        <>
+            <nav class="navbar navbar-expand bg-body-secondary">
+                <div class="container-fluid">
+                    <div className='row justify-content-between'>
+                        <div className='col-4'>
+                    <a class="navbar-brand" href="#">STAR WARS</a>
+                    </div>
+                    <div className='col-4'>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle bg-primary text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Favorites {store?.favorites?.length}
+                                </a>
+                                <ul class="dropdown-menu">
+                                    {store?.favorites?.map(({name}, index) => {
+                                        return (
+                                            <li key={index}>{name}<button className='btn btn-danger' onClick={() => actions.deleteFavorites(index)}>Delete</button></li>
+                                        )
+                                    })}
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </nav>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/detailedpeople/:url' element={<DetailedPeople />}/>
+                    <Route path='/detailedplanets/:url' element={<DetailedPlanets />}/>
+                    <Route path='/detailedvehicles/:url' element={<DetailedVehicles />}/>
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
-export default App;
+export default injectContext(App);
---
+++ src/store/flux.js
@@ -0,0 +1,63 @@
+/**
+ *  getStore: () => {}, //funcion que devuelve el objeto store con cada uno de los atributos alli definidos, 
+ *  getActions: () => {}, //funcion que devuelve el objeto actions con cada una de las funciones alli definidas,
+ *  setStore: () => {} // funcion que recibe como argumento un objeto con cada uno de los atributos que se desea actulizar o asignar un valor 
+ */
+const getStore = ({ getStore, getActions, setStore }) => {
+    return {
+        store: {
+            people: null,
+            planets: null,
+            vehicles: null,
+            favorites: [] ,
+            details: null
+        },
+        actions: {
+            getPeople: () => {
+                fetch("https://swapi.dev/api/people/")
+                    .then(response => response.json())
+                    .then(datos => setStore({ people: datos }))
+            },
+            getPlanets: () => {
+                fetch("https://swapi.dev/api/planets/")
+                    .then(response => response.json())
+                    .then(datos => setStore({ planets: datos }))
+            },
+            getVehicles: () => {
+                fetch("https://swapi.dev/api/vehicles/")
+                    .then(response => response.json())
+                    .then(datos => setStore({ vehicles: datos }))
+            },
+            getDetailsPeople: (url) => {
+                let urls = "https://swapi.dev/api/people/" + url
+                fetch(urls)
+                .then(response => response.json())
+                .then(datos => setStore({ details: datos }))
+            },
+            getDetailsPlanets: (url) => {
+                let urls = "https://swapi.dev/api/planets/" + url
+                fetch(urls)
+                .then(response => response.json())
+                .then(datos => setStore({ details: datos }))
+            },
+            getDetailsVehicles: (url) => {
+                let urls = "https://swapi.dev/api/vehicles/" + url
+                fetch(urls)
+                .then(response => response.json())
+                .then(datos => setStore({ details: datos }))
+            },
+            addFavorites: (name , index) => {
+                const newFavorite = {name,index}
+                const favoritos = getStore()
+                const newFavorites = [...favoritos.favorites, newFavorite]
+                setStore({favorites: newFavorites})
+            },
+            deleteFavorites: (index) => {
+                const favoritos = getStore()
+                setStore({favorites: favoritos.favorites.toSpliced(index,1)})
+            },
+        }
+    }
+}
+
+export default getStore
